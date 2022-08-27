7b724af2b405bb4c2d2b557655233fea5512d9ba
Kong2000 <andrewjamesking23@gmail.com>
2022-08-27 12:27:44 -0400

    local storage component complete

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function timeBlocks() {
     var timeBlkDiv = ""
     for (let i = 8; i <= 18; i++) {
         var timeHour = ""
+        var savedEntry = localStorage.getItem(i) || ""
         if (i > 12) {
             timeHour = (i - 12) + "pm"
         } else if (i === 12) {
@@ -11,7 +12,7 @@ function timeBlocks() {
         }
         timeBlkDiv += `<div class="row m-1 p-1">
         <h4 class="hour text-center">${timeHour}</h4>
-        <textarea id="${i}" row="2" cols="90" class="description"></textarea>
+        <textarea id="${i}" value=${savedEntry} placeholder=${savedEntry} row="2" cols="90" class="description"></textarea>
         <button class="saveBtn">Save</button>
         </div>`
     }
@@ -27,4 +28,5 @@ $(".saveBtn").on("click", function () {
     var textEntry = $(this).siblings(".description").val()
     var time = $(this).siblings(".description").attr("id")
     console.log("click",textEntry,time)
+    localStorage.setItem(time,textEntry)
 })
